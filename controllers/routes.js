02284b301e841d838007b76a4afc876e019ffeb9
Ryan Smith <ryan.l.smith4@gmail.com>
2018-12-13 17:45:52 -0800

    first commit

---
+++ controllers/routes.js
@@ -0,0 +1,17 @@
+// Init Express in this application
+
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+// Routes
+// =============================================================================
+
+    // Index Route
+    router.get('/', (req, res) => {
+        res.send({
+            message: 'Housing index page'
+        })
+    })
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,39 @@
+const debug = true;
+// Base SETUP
+// =============================================================================
+// Init Express in this application
+
+const express = require('express');
+const app = express();
+const port = 1337;
+
+// Instanciate our Router
+const router = express.Router();
+
+// Middleware
+// =============================================================================
+
+if( debug == true ) {
+
+    router.use((req, res, next) => {
+        console.log(req.method, req.url);
+
+        next()
+    });
+};
+
+// Routes
+
+const routes = require('./controllers/routes');
+// =============================================================================
+
+// Have app use routes controller
+app.use('/listings', routes);
+// Have the express object use the router
+
+// START THE SERVER
+// =============================================================================
+
+app.listen(port, () => {
+    console.log("RentMe is running on " + port);
+});
